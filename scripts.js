f4c4952547725eacb0ca42fa578956e3bb0ed911
Lucas Zamile <mkohlakalilucas7@gmail.com>
2024-09-04 14:09:09 +0200

    modified new page

--- scripts.js
+++ scripts.js
@@ -35,3 +35,82 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+
+
+
+        
+const slides = document.querySelectorAll('.slides');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+// Set initial dot opacity
+dots[0].style.opacity = '1';
+
+// Position slides
+slides.forEach((slide, i) => {
+  slide.style.left = `${i * 100}%`;
+});
+
+// Move slide function
+const moveSlide = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+};
+
+// Remove dots opacity
+const removeDotsOpacity = () => {
+  dots.forEach((dot) => {
+    dot.style.opacity = '.2';
+  });
+};
+
+// Dot click event listener
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', (e) => {
+    index = i;
+    removeDotsOpacity();
+    e.target.style.opacity = '1';
+    moveSlide();
+  });
+});
+
+// Previous button click event listener
+prevBtn.addEventListener('click', () => {
+  if (index === 0) return;
+  index--;
+  removeDotsOpacity();
+  dots[index].style.opacity = '1';
+  moveSlide();
+});
+
+// Next button click event listener
+nextBtn.addEventListener('click', () => {
+  if (index === slides.length - 1) return;
+  index++;
+  removeDotsOpacity();
+  dots[index].style.opacity = '1';
+  moveSlide();
+});
+
+// Auto-play slide function
+const autoPlaySlide = () => {
+  removeDotsOpacity();
+  if (index === slides.length - 1) index = -1;
+  index++;
+  dots[index].style.opacity = '1';
+  moveSlide();
+};
+
+// Auto-play slide on window load
+window.onload = () => {
+  setInterval(autoPlaySlide, 6000);
+};
+
+
